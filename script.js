506c89fbd83bb463d6b77f99bbd2ca8f5aede5e8
Joe Minor <joeminor8533@gmail.com>
2023-08-01 11:21:27 -0500

    Still working on validation

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 
 window.addEventListener("load", function() {
 
-    let form = document.querySelector('formsubmit');
    let listedPlanets;
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
    let listedPlanetsResponse;
@@ -13,16 +12,6 @@ window.addEventListener("load", function() {
        console.log(listedPlanets);
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
    })
-   form.addEventListener("submit", function(event) {
-    event.preventDefault();
-    let pilotnameInput = document.querySelector("input[name='pilotname']");
-    let copilotnameInput = document.querySelector("input[name='copilotname']");
-    let fuellevelInput = document.querySelector("input[name='fuellevel']");
-    let cargomassInput = document.querySelector("input[name='cargomass']");
-    if (pilotusernameInput.value === "" || copilotnameInput.value === "" || fuellevelInput.value === "" || cargomassInput.value === "") {
-    alert("All fields are required.");
-    console.log(alert)
-    }
-   })
+  
    
 });
